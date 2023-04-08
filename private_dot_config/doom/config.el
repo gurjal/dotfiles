91627a215e733714c3d341e2e3c42abfb0bb33d0
gurjal <gurjal@proton.me>
2023-04-07 17:06:06 -0700

    update

--- private_dot_config/doom/config.el
+++ private_dot_config/doom/config.el
@@ -1,85 +1,19 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+(setq user-full-name "shaan raya"
+      user-mail-address "shaanraya@proton.me"
+      doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 15 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "JetBrainsMono Nerd Font")
+      doom-big-font-increment 2
+      doom-unicode-font  (font-spec :family "Noto Color Emoji")
+      doom-theme 'doom-tokyo-night
+      display-line-numbers-type 'relative
+      vimish-fold-global-mode 1
+      confirm-kill-emacs nil
+      confirm-kill-processes nil)
 
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-(setq doom-font (font-spec :family "Iosevka Nerd Font" :size 18 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "Iosevka Nerd Font" :size 18))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-horizon)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type 'relative)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/.gurjal/org/")
-(setq org-roam-directory "~/.gurjal/zettelkasten")
-
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-;; vimish folds
-(setq vimish-fold-global-mode 1)
+;; unbind evil-snipe-mode from default 's' key
+(remove-hook 'doom-first-input-hook #'evil-snipe-mode)
 
 ;; zen mode
 (after! writeroom-mode
@@ -93,39 +27,34 @@
     (when (bound-and-true-p +line-num--was-activate-p)
       (display-line-numbers-mode +line-num--was-activate-p))))
 
-;; unbind evil-snipe-mode from default 's' key
-(remove-hook 'doom-first-input-hook #'evil-snipe-mode)
+;; evil-escape
+(setq-default evil-escape-key-sequence "jk" evil-escape-delay 0.2)
+(after! evil-escape (delete 'vterm-mode evil-escape-excluded-major-modes))
 
-;; gurjal mappings
-(map!
- ;; wtf is going on with tab?
- ;; :n     "<tab>" #'c-indent-line-or-region
- ;; lsp
- ;; :n     "C-n"   #'next-error
- ;; :n     "C-p"   #'previous-error
- ;; goto
- :nv    "s"     #'avy-goto-char-2
- :nv    "gh"    #'evil-beginning-of-line
- :nv    "gj"    #'evil-goto-line
- :nv    "gk"    #'evil-goto-first-line
- :nv    "gl"    #'evil-end-of-line
- ;; notes
- :n     "-"     #'org-mark-ring-goto
- ;; search
- :n     "gw"   #'evil-ex-search-word-forward
- :n     "gW"   #'evil-ex-search-word-backward
- ;; buffer
- :n     "\\"    #'evil-next-buffer
- :n     "|"     #'evil-prev-buffer
- :n     "C-n"   #'evil-next-buffer
- :n     "C-p"   #'evil-prev-buffer
- ;; macro
- :n     "Q"     #'call-last-kbd-macro
- ;; folds
- :n     "z,"    #'vimish-fold-from-marks)
+;; gurjal
+(map! :nv    "s"     #'avy-goto-char-2
+      :nv    "gh"    #'evil-beginning-of-line
+      :nv    "gj"    #'evil-goto-line
+      :nv    "gk"    #'evil-goto-first-line
+      :nv    "gl"    #'evil-end-of-line
+      ;; search
+      :n     "gw"   #'evil-ex-search-word-forward
+      :n     "gW"   #'evil-ex-search-word-backward
+      ;; buffer
+      :n     "C-j"   #'evil-next-buffer
+      :n     "C-k"   #'evil-prev-buffer
+      ;; macro
+      :n     "Q"     #'call-last-kbd-macro
+      ;; folds
+      :n     "z,"    #'vimish-fold-from-marks
+      ;; toggle
+      :leader :prefix "t"
+      :n     "s"     #'scroll-lock-mode)
 
-;; zettelkasten
-(setq org-roam-capture-templates
+;; org templates
+(setq org-directory "~/.gurjal/org/"
+      org-roam-directory "~/.gurjal/zettelkasten"
+      org-roam-capture-templates
       '(("n" "node" plain
          "%a\n* %?"
          :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
@@ -138,11 +67,12 @@
          "%?"
          :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
          :unnarrowed t)))
-(map! :leader
-      (:prefix-map ("v" . "visual")
-                   "s" #'scroll-lock-mode))
-(map! :leader
-      (:prefix-map ("j" . "zettelkasten")
+;; org default fold level
+(after! org (setq org-startup-folded 'show2levels))
+;; org mappings
+(map! :n     "-"     #'org-mark-ring-goto
+      :leader
+      (:prefix-map ("j" . "journal")
                    "g" #'org-roam-dailies-goto-today
                    "f" #'org-roam-node-find
                    "F" #'org-roam-ref-find
